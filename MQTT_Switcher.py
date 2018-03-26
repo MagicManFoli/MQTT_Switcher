9fbda330480bd8bf7ec77a4aaa15e7554551737e
MMF <magicmanfoli@gmail.com>
2018-03-26 21:05:52 +0200

    updated logger

--- MQTT_Switcher.py
+++ MQTT_Switcher.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#author: MF
+# author: MF
 # listens to MQTT and switches 433MHz radio sockets
 
 import paho.mqtt.client as mqtt
@@ -7,32 +7,36 @@ import subprocess
 
 import logging
 
+formatter = "[%(asctime)s][%(levelname)s]: %(message)s"
+
 # DEFINE RADIO SOCKETS HERE
 syscode = "01010"
-radio_id = {"Lamp" : 1}
+radio_id = {"Lamp": 1}
 
 
 def on_con(client, userdata, flags, rc):
     logging.info("Connected to MQTT-Broker with Code " + str(rc))
-    
+
     # subscribe here to renew on connection reset
     client.subscribe("Switches/#", 2)
+
 
 def on_msg(client, userdata, msg):
     logging.debug("msg received on topic <{}> with payload <{}>".format(msg.topic, msg.payload))
     subtopic = msg.topic.split("/")[1]
     state = msg.payload.decode("utf-8")
 
-    logging.debug("Calling Socket for subtopic '{}' with state '{}'".format(subtopic, state)) 
+    logging.debug("Calling Socket for subtopic '{}' with state '{}'".format(subtopic, state))
     exec_send(radio_id[subtopic], state)
+
 
 def exec_send(number, state):
     logging.info("sending state {} for device {} in system {} via 433MHz".format(state, number, syscode))
-    
+
     # SEND
     try:
         bash_command = "/home/pi/raspberry-remote/send {} {} {}".format(syscode, number, state)
-        subprocess.check_call(bash_command.split(), timeout = 10)
+        subprocess.check_call(bash_command.split(), timeout=10)
     except subprocess.TimeoutExpired:
         logging.error("sending failed, timeout")
         return
@@ -42,28 +46,26 @@ def exec_send(number, state):
 
     logging.debug("message was sent")
 
+
 def main():
     print("Starting sub_Switch, a tool to transfer MQTT commands to 433MHz")
-    
-    logging.basicConfig(filename='/home/pi/sub_Switch.log', filemode='w', level=logging.DEBUG)
+
+    logging.basicConfig(filename='/home/pi/sub_Switch.log', filemode='w', level=logging.DEBUG, format=formatter)
     logging.info("Initialising... ")
 
     client = mqtt.Client()
-    
+
     client.on_connect = on_con
     client.on_message = on_msg
 
     client.connect("192.168.178.101", 1883, 60)
-    
+
     logging.info("starting server")
     # starting blocking execution
     client.loop_forever()
-    
-    #never getting here
+
+    # never getting here
     return 0
 
 
 main()
-
-
-
