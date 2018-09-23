8841f7d126cb17e3812cf57f1e4c11a8bab26c9d
MMF <magicmanfoli@gmail.com>
2018-09-23 21:25:25 +0200

    updated style to modern standards, refactored mapping into separate module

--- MQTT_Switcher.py
+++ MQTT_Switcher.py
@@ -2,73 +2,126 @@
 # author: MF
 # listens to MQTT and switches 433MHz radio sockets
 
-import paho.mqtt.client as mqtt
 import subprocess
-
+import re
 import logging
+from logging import Logger
+from logging.handlers import RotatingFileHandler
+from typing import Dict
+
+import paho.mqtt.client as mqtt
+
+import mappings
 
 formatter = "[%(asctime)s][%(levelname)s]: %(message)s"
+rf_pattern = re.compile("[01]{5} [1-5]")
+root_topic = "Switches/#"
 
-# DEFINE RADIO SOCKETS HERE
-syscode = "01010"
-radio_id = {"Lamp": 1}
-
-
-def on_con(client, userdata, flags, rc):
-    logging.info("Connected to MQTT-Broker with Code " + str(rc))
-
-    # subscribe here to renew on connection reset
-    client.subscribe("Switches/#", 2)
+project_name = "MQTT_Switcher"
 
 
-def on_msg(client, userdata, msg):
-    logging.debug("msg received on topic <{}> with payload <{}>".format(msg.topic, msg.payload))
-    subtopic = msg.topic.split("/")[1]
-    state = msg.payload.decode("utf-8")
+class Bridge:
 
-    logging.debug("Calling Socket for subtopic '{}' with state '{}'".format(subtopic, state))
-    exec_send(radio_id[subtopic], state)
+    def __init__(self, logger: Logger, mapping: Dict[str, str]):
+        self._logger = logger
+        self._mappings = mapping
 
+        self.check_validity(mapping)
 
-def exec_send(number, state):
-    logging.info("sending state {} for device {} in system {} via 433MHz".format(state, number, syscode))
+        self._client = mqtt.Client()
+        self._client.on_connect = self._on_con
+        self._client.on_message = self._on_msg
 
-    # SEND
-    try:
-        bash_command = "/home/pi/raspberry-remote/send {} {} {}".format(syscode, number, state)
-        subprocess.check_call(bash_command.split(), timeout=10)
-    except subprocess.TimeoutExpired:
-        logging.error("sending failed, timeout")
-        return
-    except subprocess.CalledProcessError:
-        logging.error("sending failed, process return not null")
-        return
+    @staticmethod
+    def check_validity(mapping: Dict[str, str]):
+        """
+        Used to prevent dict-key exploits when executing shell command
+        """
+        for value in mapping.values():
+            if rf_pattern.match(value) is None:
+                raise ValueError(value)
 
-    logging.debug("message was sent")
+        # any(rf_pattern.match(v) is None for v in mappings.TtoID.values())
+
+    def _on_con(self, client, userdata, flags, rc):
+        self._logger.debug(f"Connected to MQTT-Broker with Code {rc}")
+
+        # subscribe here to renew on connection reset
+        client.subscribe(root_topic, 2)
+
+    def _on_msg(self, client, userdata, msg):
+        self._logger.debug(f"msg received on topic <{msg.topic}> with payload <{msg.payload}>")
+        subtopic = msg.topic.split("/")[1]
+        state = msg.payload.decode("utf-8")
+
+        self._logger.debug(f"Subtopic '{subtopic}', state '{state}'")
+        self._exec_send(self._mappings[subtopic], state)
+
+    def _exec_send(self, rf_id, state):
+        self._logger.info("sending state {state} for 433 MHz ID {rf_id}")
+
+        try:
+            bash_command = f"/home/pi/raspberry-remote/send {rf_id} {state}"
+            subprocess.check_call(bash_command.split(), timeout=10)
+        except subprocess.TimeoutExpired:
+            self._logger.error("sending failed, timeout")
+            return
+        except subprocess.CalledProcessError as e:
+            self._logger.error(f"sending failed, process returned {e}")
+            return
+
+        self._logger.debug("message was sent")
+
+    def start(self):
+        self._client.connect("192.168.178.101", 1883, 60)
+
+        self._logger.info("Starting bridge [blocking]")
+        # starting blocking execution
+        self._client.loop_forever()
 
 
 def main():
-    print("---------------------------------------------------------------")
-    print("Starting sub_Switch, a tool to transfer MQTT commands to 433MHz")
+    logger = get_logger(project_name)
+    logger.info(f" --- Starting {project_name}, a tool to transfer MQTT commands to 433MHz --- ")
 
-    logging.basicConfig(filename='/home/pi/MQTT_Switcher/MQTT_Switcher.log', filemode='a', level=logging.DEBUG, format=formatter)
-    logging.info("Initialising... ")
-
-    client = mqtt.Client()
-
-    client.on_connect = on_con
-    client.on_message = on_msg
-
-    client.connect("192.168.178.101", 1883, 60)
-
-    logging.info("starting server")
-    # starting blocking execution
-    client.loop_forever()
-
-    print("main is done, this should never happen")
+    bridge = Bridge(logger, mappings.TtoID)
+    # start [blocking]
+    bridge.start()
 
     # never getting here
-    return 0
+    raise RuntimeError("Unexpected termination")
 
 
-main()
+def get_logger(name: str):
+    """
+    prepare logging to file & stream. Can be called multiple times .
+    :return: logger object
+    """
+
+    # singleton, value only present if previously executed
+    if "logger" in get_logger.__dict__:
+        get_logger.logger.warning("Prevented double initialisation of logger")
+        return get_logger.logger
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)  # minimum level
+
+    # complete log in file
+    fh = logging.handlers.RotatingFileHandler(f"{name}.log", maxBytes=1 * 1024 * 1024, backupCount=4)  # 1MB
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(fh)
+
+    # logging >=info to stdout
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    logger.addHandler(ch)
+
+    get_logger.logger = logger
+
+    return logger
+
+
+if __name__ == '__main__':
+    main()
---
+++ mappings.py
@@ -0,0 +1,5 @@
+# variable parts of Switcher
+# MODIFY TO ADD ENTRIES!
+
+
+TtoID = {"living": "01010 1", "office": "01010 2", "printer": "01010 5"}
